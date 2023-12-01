edd3d12111e3b7f784dfa28bab52f08f6633f242
Muhammad Aziz Prasetyo <azizprasetyo100901@gmail.com>
2023-12-02 05:33:00 +0700

    Create routes and simple controller.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,42 @@
 import {Elysia} from "elysia";
 
-const app = new Elysia().get("/", () => "Hello, Bun Developer! I am gonna build RESTful APIs 😎").listen(3000);
+const app = new Elysia()
+  .get("/", () => "Hello, Bun Developer! I am gonna build RESTful APIs 😎")
+  .get("/posts/:id", ({ params: { id } }) => {
+    return {
+      id: id,
+      title: "Something cool about Bun!"
+    }
+  })
+  .get("/tracks", () => {
+    // return new Response(JSON.stringify({
+    //   "tracks": [
+    //     "Monkey Dancing",
+    //     "Joji",
+    //     "Pinkguy"
+    //   ]
+    // }), {
+    //   headers: {
+    //     "Content-Type": "application/json"
+    //   }
+    // })
+    // samething like here:
+    return {
+      "tracks": [
+        "Monkey Dancing",
+        "Joji",
+        "Pinkguy",
+        "A new song"
+      ]
+    }
+  })
+  .get("/tracks/*", () => "This is track route!")
+  .post("/posts", ({ body, set }) => {
+    set.status = 201
+
+    return body
+  })
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
